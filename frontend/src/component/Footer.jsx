02c08c5980d1034ff9de9e21957a342009ada011
xudongWu2022 <130894014+xudongWu2022@users.noreply.github.com>
2024-02-19 17:28:48 -0800

    Add files via upload

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,18 @@
+
+import { Container,Row,Col } from 'react-bootstrap'
+const Footer = () => {
+  const currentYear = new Date().getFullYear()
+    return (
+    <Footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                    <p>Proshop &copy;{currentYear}</p>
+                </Col>
+            </Row>
+        </Container>
+    </Footer>
+  )
+}
+
+export default Footer
